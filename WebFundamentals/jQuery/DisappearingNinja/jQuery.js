9f898c3acaf3445f0ddee919b9c9902453059734
Lizhang_Xie <lizhang.xie@gmail.com>
2017-06-15 09:12:22 -0500

    Revert "init a new repo for my DojoAssignments"
    This reverts commit b09d069f4f43b888065859cbb50146cb992ec57c.

--- WebFundamentals/jQuery/DisappearingNinja/jQuery.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('img').click(function() {
-        $(this).hide("slow");
-    })
-    $('button').click(function () {
-        $('img').show("slow");
-    })
-});
