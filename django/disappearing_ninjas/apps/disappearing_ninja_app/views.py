4408e099c2128ac33bbff52b74b74bb46302af84
Lizhang_Xie <lizhang.xie@gmail.com>
2017-06-19 21:21:52 -0500

    added dissapearing_ninjas(finished)

--- django/disappearing_ninjas/apps/disappearing_ninja_app/views.py
+++ django/disappearing_ninjas/apps/disappearing_ninja_app/views.py
@@ -8,6 +8,17 @@ def ninjas(request):
     return render(request,'disappearing_ninja_app/ninjas.html')
 
 def color(request,ninja_color):
-    
-        
+    if ninja_color == 'blue':
+        image = 'leonardo.jpg'
+    elif ninja_color == 'orange':
+        image = 'michelangelo.jpg'
+    elif ninja_color == 'red':
+        image = 'raphael.jpg'
+    elif ninja_color == 'purple':
+        image = 'donatello.jpg'
+    else:
+        image = 'notapril.jpg'
+    context = {
+            'image': 'disappearing_ninja_app/img/' + image,
+        }
     return render(request, 'disappearing_ninja_app/display_color.html', context)
