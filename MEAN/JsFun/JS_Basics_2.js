bfcb2e948ed881f79157e2eb03f88643504ef420
Lizhang_Xie <lizhang.xie@gmail.com>
2017-07-03 14:55:29 -0500

    added JS_Scroll.html

---
+++ MEAN/JsFun/JS_Basics_2.js
@@ -0,0 +1,13 @@
+function magic_multiply(x, y){
+    // --- your code here ---
+    let a = x*y;
+
+    return a;
+}
+
+console.log(magic_multiply(5,2))
+
+var x = "i like coding"
+for (var i = 0; i < x.length; i++) {
+    console.log(x[i]);
+}
