42eca3cef72f2deb58f4f74c6320e99c11c2355b
Lizhang_Xie <lizhang.xie@gmail.com>
2017-08-07 16:02:24 -0500

    added dojo_dashboard_2

--- Ruby/rails/dojo_dashboard_1/app/controllers/dojos_controller.rb
+++ Ruby/rails/dojo_dashboard_1/app/controllers/dojos_controller.rb
@@ -3,4 +3,25 @@ class DojosController < ApplicationController
       @dojos = Dojo.all
     render 'index'
   end
+
+  def new
+    render 'new'
+  end
+
+  def create
+    dojo = Dojo.create(dojo_param)
+    if dojo.valid?
+      flash[:success] = "Succuessfully created."
+      return redirect_to '/'
+    else
+      flash[:error] = dojo.errors
+      redirect_to '/dojos/new'
+    end
+  end
+
+
+  private
+    def dojo_param
+      params.require(:dojo).permit(:branch, :street, :city, :state)
+    end
 end
--- Ruby/rails/dojo_dashboard_1/config/routes.rb
+++ Ruby/rails/dojo_dashboard_1/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "dojos#index"
+  post "dojos" => "dojos#create"
+  get "dojos/new" => "dojos#new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
