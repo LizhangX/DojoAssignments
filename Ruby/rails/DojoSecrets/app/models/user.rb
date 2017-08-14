2c0de1e9a1dd3706a3da32af2a594272a21c492a
Lizhang_Xie <lizhang.xie@gmail.com>
2017-08-14 15:03:31 -0500

    added secrets--DojoSecret

--- Ruby/rails/DojoSecrets/app/models/user.rb
+++ Ruby/rails/DojoSecrets/app/models/user.rb
@@ -5,6 +5,10 @@ class User < ActiveRecord::Base
   validates :email, format: { with: /(\A([a-z]*\s*)*\<*([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\>*\Z)/i }
   before_save :downcase_email
 
+  has_many :secrets
+  has_many :likes, dependent: :destroy
+  has_many :secrets_liked, through: :likes, source: :secret
+
   def downcase_email
     self.email.downcase!
   end
