424878ec9f186550807b04715e41793be5b5916a
Lizhang_Xie <lizhang.xie@gmail.com>
2017-08-15 01:31:20 -0500

    added secrets auth

--- Ruby/rails/DojoSecrets/app/controllers/secrets_controller.rb
+++ Ruby/rails/DojoSecrets/app/controllers/secrets_controller.rb
@@ -11,7 +11,7 @@ class SecretsController < ApplicationController
   end
 
   def destroy
-    # return redirect_to "users/#{current_user.id}" unless params[:id] == current_user.id
+    return redirect_to "users/#{current_user.id}" unless params[:id] == current_user.id.to_s
     Secret.find(params[:id]).destroy
     return redirect_to "/users/#{current_user.id}"
   end
